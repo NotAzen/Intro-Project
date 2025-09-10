fd0d21c3a13e02b44890f19f7195a397fa457ba5
MUKILTEO\1725375 <azenosnothere@gmail.com>
2025-09-10 09:39:49 -0700

    adding literally everything from the project

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // initialize variables
+    public GameObject player;
+    Vector3 cameraPos, transformNeeded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cameraPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // vector from camera to player (?)
+        transformNeeded.x = cameraPos.x - player.transform.position.x;
+        transformNeeded.y = cameraPos.y - player.transform.position.y;
+        cameraPos = cameraPos - transformNeeded / 200;
+
+        // actually assign camera position the cameraPos variable
+        transform.position = cameraPos;
+
+        //print(transform.position + ", " + cameraPos + ", " + player.transform.position);
+    }
+}
