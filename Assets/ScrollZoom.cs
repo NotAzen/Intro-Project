78306d72b9a7b74497b89f1e1d9cd21c469de6e8
MUKILTEO\1725375 <azenosnothere@gmail.com>
2025-09-11 08:03:40 -0700

    added camera scrolling

---
+++ Assets/ScrollZoom.cs
@@ -0,0 +1,30 @@
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class ScrollZoom : MonoBehaviour
+{
+    public Camera CameraZoom;
+    float predictedZoom;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollInput != 0)
+        {
+            predictedZoom = CameraZoom.orthographicSize - 3 * scrollInput;
+
+            // change 
+            if (10 > predictedZoom && predictedZoom > 5) {
+                CameraZoom.orthographicSize -= 3*scrollInput;
+            }
+        }
+    }
+}
